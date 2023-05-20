4414a1938a0bf056bc5d3ed21f79d82062ae4685
unknown <imtiazzahir00@gmail.com>
2023-05-20 16:43:29 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+window.addEventListener("scroll", () => {
+    document.getElementById("nav").classList.toggle('scroll', window.scrollY > 0)
+});
+
+// var img = document.querySelectorAll(".notice-image")
+// var click = document.querySelectorAll(".notice-name")
+
+// click.addEventListener('click', function click() {
+//     // get.style = "visibility: visible";
+//     console.log("dksfldf");
+// })
+
+
+                       
